a61a52236ec2bbcbb96842a3c8c4806cb487d9a1
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2025-07-10 16:09:36 -0500

    123

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlides() {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+  });
+  slideIndex = (slideIndex + 1) % slides.length;
+  slides[slideIndex].classList.add('active');
+}
+
+setInterval(showSlides, 5000);
